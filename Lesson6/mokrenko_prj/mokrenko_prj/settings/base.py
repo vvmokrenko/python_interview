758db8c61f407fe9ee9b70f069a81377e7e89963
vvmokrenko <v.v.mokrenko@gmail.com>
2021-12-02 20:37:18 +0300

    lesson 7 homework

--- Lesson6/mokrenko_prj/mokrenko_prj/settings/base.py
+++ Lesson6/mokrenko_prj/mokrenko_prj/settings/base.py
@@ -130,9 +130,11 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
+
+STATIC_ROOT = BASE_DIR / '..' / 'nginx' / 'static'
 
 #
 # try:
